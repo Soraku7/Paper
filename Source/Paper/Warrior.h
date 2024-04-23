928f6665231756d834fbb35de43b4f17807211e6
Soraku7 <2673140287@qq.com>
2024-04-24 01:11:33 +0800

    Anim and Character

---
+++ Source/Paper/Warrior.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "Warrior.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAPER_API AWarrior : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
