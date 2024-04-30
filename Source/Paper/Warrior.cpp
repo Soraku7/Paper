921d6356d3b814357928febc2a56734bd7800abd
Soraku7 <2673140287@qq.com>
2024-04-30 11:59:36 +0800

    Move

--- Source/Paper/Warrior.cpp
+++ Source/Paper/Warrior.cpp
@@ -3,3 +3,30 @@
 
 #include "Warrior.h"
 
+#include "PaperFlipbookComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AWarrior::AWarrior()
+{
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera"));
+	SpringArmComponent -> SetupAttachment(RootComponent);
+	SpringArmComponent -> TargetArmLength = 900.0f;
+	SpringArmComponent -> SetRelativeLocation(FVector(0.0f , 0.0f , 40.f));
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Boom"));
+	CameraComponent -> SetupAttachment(SpringArmComponent);
+	
+	GetSprite() -> SetCastShadow(true);
+}
+
+bool AWarrior::IsRunning() const
+{
+	return GetCharacterMovement() -> IsMovingOnGround() && GetVelocity().X != 0.0f;
+}
+
+void AWarrior::Move(const float InputActionValue)
+{
+	AddMovementInput(FVector::ForwardVector , InputActionValue);
+}
--- Source/Paper/Warrior.h
+++ Source/Paper/Warrior.h
@@ -6,6 +6,8 @@
 #include "PaperZDCharacter.h"
 #include "Warrior.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
 /**
  * 
  */
@@ -13,5 +15,19 @@ UCLASS()
 class PAPER_API AWarrior : public APaperZDCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	AWarrior();
+
+	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
+	bool IsRunning() const;
+private:
+	UPROPERTY(VisibleAnywhere , Category = "Components" , meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere , Category = "Components" , meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComponent;
+
+	UFUNCTION(BlueprintCallable , Category = "Movement")
+	void Move(const float InputActionValue);
 };
