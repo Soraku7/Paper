912cda4aadca5c00c8883973591d76e6e3743ffe
Soraku7 <2673140287@qq.com>
2024-06-01 10:53:20 +0800

    Crouch

--- Source/Paper/Warrior.cpp
+++ Source/Paper/Warrior.cpp
@@ -14,19 +14,35 @@ AWarrior::AWarrior()
 	SpringArmComponent -> SetupAttachment(RootComponent);
 	SpringArmComponent -> TargetArmLength = 900.0f;
 	SpringArmComponent -> SetRelativeLocation(FVector(0.0f , 0.0f , 40.f));
+	SpringArmComponent ->bInheritYaw = false;
 
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Boom"));
 	CameraComponent -> SetupAttachment(SpringArmComponent);
 	
 	GetSprite() -> SetCastShadow(true);
+
+	GetCharacterMovement() -> bOrientRotationToMovement = true;
+	GetCharacterMovement() -> RotationRate = FRotator(0.0f , 10000.0f , 0.0f);
 }
 
-bool AWarrior::IsRunning() const
+void AWarrior::BeginPlay()
 {
-	return GetCharacterMovement() -> IsMovingOnGround() && GetVelocity().X != 0.0f;
+	Super::BeginPlay();
+}
+
+bool AWarrior::IsGrounded() const
+{
+	return GetCharacterMovement()->IsMovingOnGround();
+}
+
+bool AWarrior::IsCorouching() const
+{
+	return GetCharacterMovement() -> IsCrouching();
 }
 
 void AWarrior::Move(const float InputActionValue)
 {
 	AddMovementInput(FVector::ForwardVector , InputActionValue);
+
+	SetMoveInputValue(InputActionValue);
 }
--- Source/Paper/Warrior.h
+++ Source/Paper/Warrior.h
@@ -19,8 +19,8 @@ class PAPER_API AWarrior : public APaperZDCharacter
 public:
 	AWarrior();
 
-	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
-	bool IsRunning() const;
+	virtual void BeginPlay() override;
+ 
 private:
 	UPROPERTY(VisibleAnywhere , Category = "Components" , meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* SpringArmComponent;
@@ -28,6 +28,26 @@ private:
 	UPROPERTY(VisibleAnywhere , Category = "Components" , meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* CameraComponent;
 
+	float MoveInputValue;
+	
+	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
+	FORCEINLINE bool IsRunning() const{ return IsGrounded() && MoveInputValue != 0.0f; }
+
+	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
+	FORCEINLINE bool IsFalling() const{return !IsGrounded() && GetVelocity().Z <= 0.0f ;}
+
+	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
+	FORCEINLINE bool IsLanding() const{return IsGrounded() && MoveInputValue == 0.0f ;}
+
+	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
+	bool IsCorouching() const;
+	
+	UFUNCTION(BlueprintPure , Category = "Anim Parameters")
+	bool IsGrounded() const;
+	
 	UFUNCTION(BlueprintCallable , Category = "Movement")
 	void Move(const float InputActionValue);
+	
+	UFUNCTION(BlueprintCallable , Category = "Movement")
+	FORCEINLINE void SetMoveInputValue(const float InValue) {MoveInputValue = InValue;}
 };
